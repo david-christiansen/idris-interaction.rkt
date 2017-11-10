3dc914b7db759b8b70b25e20e16a2c057708d0e1
David Christiansen <david@davidchristiansen.dk>
2017-11-10 16:23:34 -0500

    Better comments in slideshow example

--- example-slideshow.rkt
+++ example-slideshow.rkt
@@ -19,17 +19,20 @@
          (idris-slideshow-commands
           (list (list "Vect.(++) in Idris" ":printdef Vect.(++)")
                 (list "Vect.(++) in TT" ":core Vect.(++)"))
+          ;; Use this font for the code. It looks best with a mono font.
           #:face "DejaVu Sans Mono"
           ;; Font size
           #:size 32
           ;; This is the module context within which the commands are executed
           #:preamble "module Slide\n\nimport Data.Vect\n\n"
+          ;; The width of the widget in the slide.
           #:width 1000))
 
 ;; Put Idris code in a slide. The keybindings in the slide editor are
 ;; a subset of those in idris-mode for Emacs.
 (slide #:title "Interactive Idris stuff"
        (idris-slideshow-editor
+        ;; The contents of the code box in the slide
         (string-join
          '("foo : Nat -> Nat"
            "foo x = ?huh"
@@ -41,7 +44,9 @@
         #:size 25
         ;; Show an interactive REPL in addition to the code editor
         #:repl? #t
+        ;; Before type checking, this will be prepended to the editor.
         #:preamble "import Data.Fin"
+        ;; Type check the contents immediately when the slide appears?
         #:auto-load? #t))
 
 (slide
